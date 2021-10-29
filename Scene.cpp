f85c8b20085147a7f68093213ae6b015bb2e68a3
leesunho96 <tjsgh0728@naver.com>
2021-10-29 17:59:33 +0900

    1029시작

--- Scene.cpp
+++ Scene.cpp
@@ -1,41 +1,82 @@
 #include "stdafx.h"
-#include "Device.h"
-#include "Player.h"
+#include "Systems/Device.h"
 
-Player* player;
-float speed = 50.0f;
+struct Vertex
+{
+	D3DXVECTOR3 Position;
+	D3DXVECTOR2 Uv;
+};
+ID3D11Buffer* vertexBuffer;
+
+Shader* shader;
+ID3D11ShaderResourceView* srv;
+
 void InitScene()
 {
-	player = new Player(L"../_Shaders/006_Rect.fx");
-	player->Scale(100, 100);
+	Vertex vertices[6];
+	vertices[0].Position = D3DXVECTOR3(-0.5f, -0.5f, 0.0f);
+	vertices[1].Position = D3DXVECTOR3(-0.5f, +0.5f, 0.0f);
+	vertices[2].Position = D3DXVECTOR3(+0.5f, -0.5f, 0.0f);
+	vertices[3].Position = D3DXVECTOR3(+0.5f, -0.5f, 0.0f);
+	vertices[4].Position = D3DXVECTOR3(-0.5f, +0.5f, 0.0f);
+	vertices[5].Position = D3DXVECTOR3(+0.5f, +0.5f, 0.0f);
 
-	float x = (float)Width * 0.5f - player->Scale().x * 0.5f;
-	float y = player->Scale().y * 0.5f; // 중심 기준 바닥까지 거리가 scale의 절반.
+	vertices[0].Uv = D3DXVECTOR2(0, 1);
+	vertices[1].Uv = D3DXVECTOR2(0, 0);
+	vertices[2].Uv = D3DXVECTOR2(1, 1);
+	vertices[3].Uv = D3DXVECTOR2(1, 1);
+	vertices[4].Uv = D3DXVECTOR2(0, 0);
+	vertices[5].Uv = D3DXVECTOR2(1, 0);
 
-	player->Position(x, y);
+
+	//Create Vertex Buffer
+	{
+		D3D11_BUFFER_DESC desc = { 0 };
+		desc.Usage = D3D11_USAGE_DEFAULT;
+		desc.ByteWidth = sizeof(Vertex) * 6;
+		desc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+
+		D3D11_SUBRESOURCE_DATA data = { 0 };
+		data.pSysMem = vertices;
+
+		HRESULT hr = Device->CreateBuffer(&desc, &data, &vertexBuffer);
+		assert(SUCCEEDED(hr));
+	}
+
+	//Create SRV
+	{
+		HRESULT hr = D3DX11CreateShaderResourceViewFromFile
+		(
+			Device, L"../_Textures/Single.png", NULL, NULL, &srv, NULL
+		);
+		assert(SUCCEEDED(hr));
+	}
+
+	shader = new Shader(L"../_Shaders/007_Texture.fx");
+	shader->AsShaderResource("Map")->SetResource(srv);
+
 }
 
 void DestroyScene()
 {
-	SAFE_DELETE(player);
+	SAFE_DELETE(shader);
+
+	SAFE_RELEASE(srv);
+	SAFE_RELEASE(vertexBuffer);
 
 }
-
 
 D3DXMATRIX V, P;
 void Update()
 {
-	if (Key->Press(VK_LEFT))
-		player->MoveLeft();
-	else if (Key->Press(VK_RIGHT))
-		player->MoveRight();
+	D3DXMATRIX W, S, T;
 
-	if (Key->Down(VK_SPACE))
-		player->StartJump();
-	else if (Key->Up(VK_SPACE))
-		player->EndJump();
+	D3DXMatrixScaling(&S, 100, 100, 1);
+	D3DXMatrixTranslation(&T, 200, 200, 0);
 
+	W = S * T;
 
+	shader->AsMatrix("World")->SetMatrix(W);
 
 	//View
 	D3DXVECTOR3 eye(0, 0, -1);
@@ -46,19 +87,27 @@ void Update()
 	//Projection
 	D3DXMatrixOrthoOffCenterLH(&P, 0, (float)Width, 0, (float)Height, -1, 1);
 
+	shader->AsMatrix("View")->SetMatrix(V);
+	shader->AsMatrix("Projection")->SetMatrix(P);
 
-	player->Speed(speed);
-	player->ViewProjection(V, P);
-	player->Update();
 }
 
+
+int pass = 0;
 void Render()
 {
 	D3DXCOLOR bgColor = D3DXCOLOR(0, 0, 0, 1);
 	DeviceContext->ClearRenderTargetView(RTV, (float*)bgColor);
 	{
-		player->Render();
-		ImGui::SliderFloat("500.0f", &speed, 0.0f, 500.0f, "Speed");
+		UINT stride = sizeof(Vertex);
+		UINT offset = 0;
+
+		DeviceContext->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
+		DeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+		ImGui::SliderInt("Pass", &pass, 0, 1);
+		shader->Draw(0, pass, 6);
+
 	}
 	ImGui::Render();
 	SwapChain->Present(0, 0);
