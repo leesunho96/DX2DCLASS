b71930fe0d36cdcf82cd98b04374562cc858626a
이선호 <tjsgh0728@naver.com>
2021-12-30 19:48:07 +0900

    스프라이트 소스 추가

--- Objects/SnowBall.cpp
+++ Objects/SnowBall.cpp
@@ -15,6 +15,7 @@ void SnowBall::InitializeAnimation()
 	wstring TextureFile = Textures + L"TianSouls/SnowBall.png";
 	wstring ShaderFile = Shaders + L"009_Sprite.fx";
 
+//https://kr.123rf.com/photo_85187738_%EB%8F%85%EC%84%B1-%ED%8F%AD%EB%B0%9C-%ED%8A%B9%EC%88%98-%ED%9A%A8%EA%B3%BC-fx-%EC%95%A0%EB%8B%88%EB%A9%94%EC%9D%B4%EC%85%98-%ED%94%84%EB%A0%88%EC%9E%84-%EC%8A%A4%ED%94%84%EB%9D%BC%EC%9D%B4%ED%8A%B8-%EC%8B%9C%ED%8A%B8-%EA%B2%8C%EC%9E%84-%EB%B9%84%EB%94%94%EC%98%A4-%EB%B0%8F-%EB%A7%8C%ED%99%94%EC%97%90%EC%84%9C-%ED%94%8C%EB%9E%98%EC%8B%9C-%EC%95%A0%EB%8B%88%EB%A9%94%EC%9D%B4%EC%85%98%EC%9D%84%EC%9C%84%ED%95%9C-%EB%85%B9%EC%83%89-%EC%82%B0%EC%84%B1-%EB%8F%85%EC%84%B1-%ED%8C%8C%EC%9B%8C-%ED%8F%AD%EB%B0%9C-%ED%94%84%EB%A0%88%EC%9E%84-.html
 	// GoToTop
 	{
 		clip = new Clip(PlayMode::Loop);
